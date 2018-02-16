acfa9fd8d1b2c75d2fcdfd4f3cbcae02f5d5b885
kkiyutin <kkiyutin@pyrus.com>
2018-02-16 15:00:34 +0300

    Rebuilt all without errors

--- dist/alt.js
+++ dist/alt.js
@@ -7,7 +7,7 @@
 		exports["Alt"] = factory();
 	else
 		root["Alt"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
